eeaaa4d1093c6f6d2cba06817c256eaf7eeb1f0b
Nguyễn Thành Trung <trung.hnue@gmail.com>
2023-12-09 18:41:29 +0700

    fix board 1.6. Bitboard, update version Unity

--- 1. Bitwise Operations/1.6. Bitboard/Assets/CreateBoard.cs
+++ 1. Bitwise Operations/1.6. Bitboard/Assets/CreateBoard.cs
@@ -15,19 +15,20 @@ public class CreateBoard : MonoBehaviour
     long desertBB = 0;
     long playerBB = 0;
     long treeBB = 0;
+    const int LENGTH = 8;
     // Start is called before the first frame update
     void Start()
     {
         tiles = new GameObject[64];
-        for (int r = 0; r < tilePrefabs.Length; r++)
+        for (int r = 0; r < LENGTH; r++)
         {
-            for (int c = 0; c < tilePrefabs.Length; c++)
+            for (int c = 0; c < LENGTH; c++)
             {
                 int randomTile = UnityEngine.Random.Range(0, tilePrefabs.Length);
                 Vector3 pos = new(c, 0, r);
                 GameObject tile = Instantiate(tilePrefabs[randomTile], pos, Quaternion.identity);
                 tile.name = tile.tag + "_" + r + " " + c;
-                tiles[r * 8 + c] = tile;
+                tiles[r * LENGTH + c] = tile;
                 if (tile.CompareTag("Dirt"))
                 {
                     dirtBB = SetCellState(dirtBB, r, c);
@@ -37,22 +38,23 @@ public class CreateBoard : MonoBehaviour
                     desertBB = SetCellState(desertBB, r, c);
                 }
             }
+            PrintBB("Dirt", dirtBB);
         }
-        // PrintBB("Dirt", dirtBB);
-        // Debug.Log("Dirt cells = " + CellCount(dirtBB));
+        PrintBB("Dirt", dirtBB);
+        Debug.Log("Dirt cells = " + CellCount(dirtBB));
         InvokeRepeating(nameof(PlantTree), 1, 1);
     }
 
     void PlantTree()
     {
-        int rr = UnityEngine.Random.Range(0, 8);
-        int rc = UnityEngine.Random.Range(0, 8);
-        Debug.Log("PlantTree: " + rr + " " + rc);
+        int rr = UnityEngine.Random.Range(0, LENGTH);
+        int rc = UnityEngine.Random.Range(0, LENGTH);
+        // Debug.Log("PlantTree: " + rr + " " + rc);
 
         if (GetCellState(dirtBB & ~playerBB, rr, rc))
         {
             GameObject tree = Instantiate(treePrefab);
-            tree.transform.parent = tiles[rr * 8 + rc].transform;
+            tree.transform.parent = tiles[rr * LENGTH + rc].transform;
             tree.transform.localPosition = Vector3.zero;
             treeBB = SetCellState(treeBB, rr, rc);
         }
@@ -65,13 +67,13 @@ public class CreateBoard : MonoBehaviour
 
     long SetCellState(long bitboard, int row, int col)
     {
-        long newBit = 1L << (row * 8 + col);
+        long newBit = 1L << (row * LENGTH + col);
         return (bitboard |= newBit);
     }
 
     bool GetCellState(long bitboard, int row, int col)
     {
-        long mask = 1L << (row * 8 + col);
+        long mask = 1L << (row * LENGTH + col);
         return ((bitboard & mask) != 0);
     }
 
